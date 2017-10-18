67930e719888a477cfc0bf3cf909b42bc45189f1
tyt123 <1099499178@qq.com>
2017-10-18 18:24:14 +0800

    add dynamic navbar and linked events page to database

--- SportsRegistration/HomePage/models.py
+++ SportsRegistration/HomePage/models.py
@@ -2,5 +2,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import django.utils.timezone as timezone
 
 # Create your models here.
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+
+class Events(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30)
+    status = models.CharField(max_length=20)
+    date = models.DateTimeField(default = timezone.now)
+    content = models.CharField(max_length=300, null=True)
+
+class Sign(models.Model):
+    id = models.IntegerField(primary_key=True)
+    userid = models.IntegerField()
+    eventsid = models.IntegerField()
