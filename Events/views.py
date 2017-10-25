3eb65854aeca94b56b9ce98e545123c81b71e611
tyt123 <1099499178@qq.com>
2017-10-25 20:41:13 +0800

    update events status

--- Events/views.py
+++ Events/views.py
@@ -15,13 +15,8 @@ def index(request):
     print request.method
     events_list=list(Events.objects.all()[::-1])
     for l in events_list:
-        if l.status =="success":
-            l.s2 = "正在报名"
-            l.s3 = "立即报名"
-        elif l.status =="error":
-            l.s2 = "已截止"
-        elif l.status =="info":
-            l.s2 = "尚未开始"
+        l.s2 = gets2(l.status)
+        l.s3 = gets3(l.status)
     paginator=Paginator(events_list, 3)
     page = request.GET.get('page')
     try:
@@ -36,14 +31,39 @@ def index(request):
 
 @csrf_exempt 
 def page(request, Id):    
-    events=Events.objects.get(id=Id)
+    events = Events.objects.get(id=Id)
+    events.s2 = gets2(events.status)
+    events.s3 = gets3(events.status)
     if request.method == "POST":
         s = Sign.objects.get_or_create(userid=request.session['userid'], eventsid=Id, status=1)
         if (s[1]):
             status=1
         else:
             status=2
-        
+
         return render(request, 'Events/page.html', {'events':events, 'status':json.dumps(status)})
     else:
         return render(request, 'Events/page.html', {'events':events})
+
+
+def gets2(i):
+    if i == 1:
+        return "即将开始"
+    elif i == 2:
+        return "正在报名"
+    elif i == 3:
+        return "报名已截止"
+    elif i == 4:
+        return "比赛已结束"
+    else:
+        return "???"
+
+def gets3(i):
+    if i == 1:
+        return "info"
+    elif i == 2:
+        return "success"
+    elif i == 3:
+        return "danger"
+    elif i == 4:
+        return "warning"
--- HomePage/models.py
+++ HomePage/models.py
@@ -17,7 +17,7 @@ class User(models.Model):
 class Events(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=30)
-    status = models.CharField(max_length=20)
+    status = models.IntegerField(default=1)
     date = models.DateTimeField(default = timezone.now)
     content = models.CharField(max_length=300, null=True)
 
