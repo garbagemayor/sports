ae40fafed28ba248b68ffd8aabcd69927057867a
Shols <790479613@qq.com>
2017-10-27 10:56:19 +0800

    adjust the user config page and add some model fixes #14 fixes #29

--- HomePage/models.py
+++ HomePage/models.py
@@ -4,29 +4,41 @@ from __future__ import unicode_literals
 from django.db import models
 import django.utils.timezone as timezone
 
+
 # Create your models here.
 class User(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=30)    
+    name = models.CharField(max_length=30)
     mobile = models.CharField(max_length=30, null=True)
     fullname = models.CharField(max_length=30, null=True)
     classnumber = models.CharField(max_length=30, null=True)
-    authority = models.IntegerField(default=0)            #权限 默认无权限为0，普通管理员为1，超管为2
+    authority = models.IntegerField(default=0)  # 权限 默认无权限为0，普通管理员为1，超管为2
     email = models.CharField(max_length=30, null=True)
+    certificate_type = models.IntegerField(default=0)  # 证件类型 默认0为身份证，护照为1
+    certificate_id = models.CharField(max_length=30)
+    student_number = models.CharField(max_length=30)
+    birthday = models.DateField(unique_for_date=True)
+    CLOTH_SIZE_CHOICES = (
+        ('S', 'Small'),
+        ('M', 'Middle'),
+        ('L', 'Large'),
+    )
+    cloth_size = models.CharField(max_length=1, null=True, choices=CLOTH_SIZE_CHOICES)
+    room_address = models.CharField(max_length=30, null=True)
+
 
 class Events(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=30)
     status = models.IntegerField(default=1)
-    date = models.DateTimeField(default = timezone.now)
+    date = models.DateTimeField(default=timezone.now)
     content = models.CharField(max_length=300, null=True)
+
 
 class Sign(models.Model):
     id = models.IntegerField(primary_key=True)
     userid = models.IntegerField()
     eventsid = models.IntegerField()
-    status = models.IntegerField(default=1)     #0 未报名 1 等待审核 2 报名成功
+    status = models.IntegerField(default=1)  # 0 未报名 1 等待审核 2 报名成功
     prize = models.CharField(max_length=30, null=True)
-    time = models.DateTimeField(default = timezone.now)
-
-
+    time = models.DateTimeField(default=timezone.now)
--- Users/views.py
+++ Users/views.py
@@ -7,48 +7,56 @@ import json
 from HomePage.models import User, Sign, Events
 from django.core.paginator import Paginator
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.decorators.csrf import csrf_exempt,csrf_protect
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from django.contrib import messages
+
 
 # Create your views here.
 def auth(request):
-    r = requests.post('https://accounts.net9.org/api/access_token?client_id=0eHhovG3K1NYkhbnYuYmej1h9wY&client_secret=moK3EkYsQvossfoMwmCd&code='
-            + request.GET['code'])
+    r = requests.post(
+        'https://accounts.net9.org/api/access_token?client_id=0eHhovG3K1NYkhbnYuYmej1h9wY&client_secret'
+        '=moK3EkYsQvossfoMwmCd&code= '
+        + request.GET['code'])
     rr = requests.get('https://accounts.net9.org/api/userinfo?access_token=' + r.json()['access_token'])
-    j=rr.json()
-    user=User.objects.get_or_create(name=j['user']['name'])
+    j = rr.json()
+    user = User.objects.get_or_create(name=j['user']['name'])
     if user[1]:
-        User.objects.filter(name=j['user']['name']).update(email=j['user']['email'], mobile=j['user']['mobile'], fullname=j['user']['fullname'], classnumber=j['user']['groups'][0])
-    request.session['username']=j['user']['name']
-    request.session['userid']=user[0].id
-    request.session['auth']=user[0].authority
-    messages.add_message(request, messages.INFO, '登陆成功! '+request.session['username']+' 欢迎来到体育赛事报名平台！')
+        User.objects.filter(name=j['user']['name']).update(email=j['user']['email'], mobile=j['user']['mobile'],
+                                                           fullname=j['user']['fullname'],
+                                                           classnumber=j['user']['groups'][0])
+    request.session['username'] = j['user']['name']
+    request.session['userid'] = user[0].id
+    request.session['auth'] = user[0].authority
+    messages.add_message(request, messages.INFO, '登陆成功! ' + request.session['username'] + ' 欢迎来到体育赛事报名平台！')
     return HttpResponseRedirect("/events/")
 
+
 def logout(request):
-    if (request.session['username']):
+    if request.session['username']:
         del request.session['username']
-    if (request.session['userid']):
-        del request.session['userid']   
-    if (request.session['auth']):     
+    if request.session['userid']:
+        del request.session['userid']
+    if request.session['auth']:
         del request.session['auth']
     messages.add_message(request, messages.INFO, '成功登出！')
     return HttpResponseRedirect("/")
 
+
 def my_information(request):
     return render(request, "Users/users.html")
 
+
 def my_events(request):
-    if (request.session['userid']):
+    if request.session['userid']:
         events_list = []
         elist = Sign.objects.filter(userid=request.session['userid'])
         for e in elist:
             tmp = Events.objects.filter(id=e.eventsid)
             if tmp:
-                tmp=tmp[0]
+                tmp = tmp[0]
                 tmp.s2 = gets2(tmp.status)
                 events_list.append(tmp)
-        paginator=Paginator(events_list, 3)
+        paginator = Paginator(events_list, 3)
         page = request.GET.get('page')
         try:
             events_list = paginator.page(page)
@@ -56,32 +64,33 @@ def my_events(request):
             events_list = paginator.page(1)
         except EmptyPage:
             events_list = paginator.page(paginator.num_pages)
-        
-            
-        return render(request, 'Events/myevents.html', {'events_list':events_list})
-    else:        
+
+        return render(request, 'Events/myevents.html', {'events_list': events_list})
+    else:
         messages.add_message(request, messages.INFO, '请登录！')
         HttpResponseRedirect("/authorized/")
 
-@csrf_exempt 
-def manager(request):    
-    user=User.objects.filter().exclude(authority=0)
-    status=0
+
+@csrf_exempt
+def manager(request):
+    user = User.objects.filter().exclude(authority=0)
+    status = 0
     if request.method == "POST":
-        if len(user)<3:
-            if len(User.objects.filter(name=request.POST['name'])) == 0 :
+        if len(user) < 3:
+            if len(User.objects.filter(name=request.POST['name'])) == 0:
                 messages.add_message(request, messages.INFO, '查无此人！')
             else:
-                user=User.objects.get(name=request.POST['name'])
-                if user.authority<1:
+                user = User.objects.get(name=request.POST['name'])
+                if user.authority < 1:
                     User.objects.filter(name=request.POST['name']).update(authority=1)
-                    messages.add_message(request, messages.INFO, '成功将'+request.session['username']+'升至管理员！')
+                    messages.add_message(request, messages.INFO, '成功将' + request.session['username'] + '升至管理员！')
                 else:
                     messages.add_message(request, messages.INFO, '无此操作权限！')
-            user=User.objects.filter(authority=1)
+            user = User.objects.filter(authority=1)
         else:
             messages.add_message(request, messages.INFO, '管理员数量已达上限！')
-    return render(request, 'Users/manager.html', {'users_list':user, 'status':json.dumps(status)})
+    return render(request, 'Users/manager.html', {'users_list': user, 'status': json.dumps(status)})
+
 
 def gets2(i):
     if i == 1:
