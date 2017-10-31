a416e4439b7c1325760f61fa4284434247ddd3dd
Shols <790479613@qq.com>
2017-10-31 19:42:10 +0800

    delete some code which is useless

--- Users/user_data.py
+++ Users/user_data.py
@@ -2,9 +2,3 @@ from django.http import HttpResponse
 from  django.shortcuts import render
 from  django.views.decorators import csrf
 
-def change_code(request):
-    ctx = {}
-    if request.POST:
-            ctx['rlt'] = request.POST['old_password']
-    print ctx
-    return render(request, "Users/users.html", ctx);
