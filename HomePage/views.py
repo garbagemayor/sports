7f2cf27b5039eca94460c36c88e023dd48093f4d
tyt123 <1099499178@qq.com>
2017-11-01 19:12:33 +0800

    add link in registration_record page

--- HomePage/views.py
+++ HomePage/views.py
@@ -7,40 +7,17 @@ from HomePage.models import Events, Sign
 # Create your views here.
 
 def index(request):
-    events1=list(Events.objects.filter(status=0)[-1:-7:-1])
-    for l in events1:
-        l.s2 = gets2(l.status)
-        l.s3 = gets3(l.status)
-    paginator=Paginator(events_list, 10)
-    page = request.GET.get('page')
-    try:
-        events_list = paginator.page(page)
-    except PageNotAnInteger:
-        events_list = paginator.page(1)
-    except EmptyPage:
-        events_list = paginator.page(paginator.num_pages)
+    events1=list(Events.objects.filter(status=1)[::-1])
+    events2=list(Events.objects.filter(status=2)[::-1])
+    events3=list(Events.objects.filter(status=4)[::-1])
+    if len(events1)>5:
+        events1=events1[:5]
+    if len(events2)>5:
+        events2=events2[:5]
+    if len(events3)>5:
+        events3=events3[:5]
+
 
     
-    return render(request, 'Events/events.html', {'events_list':events_list})
+    return render(request, "HomePage/newhomepage.html", {'events1':events1, 'events2':events2, 'events3':events3})
 
-def gets2(i):
-    if i == 1:
-        return "即将开始"
-    elif i == 2:
-        return "正在报名"
-    elif i == 3:
-        return "报名已截止"
-    elif i == 4:
-        return "比赛已结束"
-    else:
-        return "???"
-
-def gets3(i):
-    if i == 1:
-        return "info"
-    elif i == 2:
-        return "success"
-    elif i == 3:
-        return "danger"
-    elif i == 4:
-        return "warning"
