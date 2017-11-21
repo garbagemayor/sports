2335315dafad65dd0bf82476bd245c5e19cf21bd
罗华一 <luohy15@mails.tsinghua.edu.cn>
2017-11-21 16:20:00 +0800

    confirm: option and correspond result. close #47

--- RegistrationRecord/forms.py
+++ RegistrationRecord/forms.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 from django import forms
 
+CONFIRM_CHOICES = (
+    (True, '通过'),
+    (False, '不通过'),
+)
 class EditForm(forms.Form):
-    title = forms.CharField(max_length=100, label='标题')
-    content = forms.CharField(widget=forms.Textarea(attrs={'id': 'editor'}), label='内容')
-    result = forms.BooleanField(required=False, label='审核结果')
+    content = forms.CharField(widget=forms.Textarea(attrs={'id': 'editor'}), label='')
+
+class OptionForm(forms.Form):
+    result = forms.ChoiceField(widget=forms.Select,
+            choices=CONFIRM_CHOICES, label='审核意见')
