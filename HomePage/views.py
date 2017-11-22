cdb7ad31f122c6c52c61ce0d67bbbfc06221ecd9
tyt123 <1099499178@qq.com>
2017-11-23 00:40:53 +0800

    fixed broadcast in homepage

--- HomePage/views.py
+++ HomePage/views.py
@@ -8,31 +8,33 @@ from HomePage.models import utcToLocal
 from django.core.paginator import Paginator
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-
 # Create your views here.
 
 def index(request):
     team = list(IMG.objects.filter(headline=True,imgtype=0))
     game = list(IMG.objects.filter(headline=True,imgtype=1))
-    celebrity = list(IMG.objects.filter(headline=True,imgtype=2))
-    print request.get_host()
-    events_list = Events.objects.all()[::-1]
-    events1 = []
-    events2 = []
-    events3 = []
+    celebrity = list(IMG.objects.filter(headline=True,imgtype=2)) 
+    broadcast_list = list(Broadcast.objects.all()[::-1])
+    events_list=Events.objects.all()[::-1]
+    events1=[]
+    events2=[]
+    events3=[]
+    
     for e in events_list:
-        if e.getStatus() == 1:
+        if e.getStatus()==1:
             events1.append(e)
-        if e.getStatus() == 2:
+        if e.getStatus()==2:
             events2.append(e)
-        if e.getStatus() == 5:
+        if e.getStatus()==5:
             events3.append(e)
-    if len(events1) > 5:
-        events1 = events1[:5]
-    if len(events2) > 5:
-        events2 = events2[:5]
-    if len(events3) > 5:
-        events3 = events3[:5]
+    if len(events1)>5:
+        events1=events1[:5]
+    if len(events2)>5:
+        events2=events2[:5]
+    if len(events3)>5:
+        events3=events3[:5]
+    if len(broadcast_list)>5:
+        broadcast_list=broadcast_list[:5]
 
     return render(request, "HomePage/newhomepage.html",
                   {'events1': events1,
@@ -43,7 +45,9 @@ def index(request):
                    'events3_len' : len(events3),
                    'team': team,
                    'celebrity': celebrity,
-                   'game': game})
+                   'game': game,
+                   'broadcast':broadcast_list})
+
 
 def broadcast(request):
     broadcast_list = list(Broadcast.objects.all()[::-1])
